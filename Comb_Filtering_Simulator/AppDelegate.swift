df35b92aba16248c7534683ece8f2d38cd1ff44f
george <george@georges-macbook-pro-2.local>
2017-10-31 11:17:18 +1000

    Initial Commit

---
+++ Comb_Filtering_Simulator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Comb_Filtering_Simulator
+//
+//  Created by george on 10/31/17.
+//  Copyright © 2017 george. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Comb_Filtering_Simulator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Comb_Filtering_Simulator
+//
+//  Created by george on 10/31/17.
+//  Copyright © 2017 george. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
